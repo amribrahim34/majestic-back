b2fa28e77dcb8f90732e0368739c6b8697389380
Amr Ibrahim <amramr3434@gmail.com>
2024-03-07 17:54:00 +0200

    force json middleware , login and register done

--- app/Http/Controllers/Website/AuthController.php
+++ app/Http/Controllers/Website/AuthController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers\Website;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Website\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str; // Add this line
+
 
 class AuthController extends Controller
 {
@@ -33,5 +39,36 @@ class AuthController extends Controller
         $request->session()->regenerateToken();
 
         return response()->json(['message' => __('messages.logout_success')], 200);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        // Validate the request data
+        $v = $request->validated();
+
+        // Create user
+        $user = $this->create($v);
+
+        // Implement any post-registration logic here, such as login or token generation
+        auth()->login($user);
+
+        // Return a successful response, e.g., user data or a redirect
+        return response()->json($user, 201);
+    }
+
+
+
+    /**
+     * Create a new user instance after a valid registration.
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'remember_token' => Str::random(10), // Or handle token generation differently
+        ]);
     }
 }
