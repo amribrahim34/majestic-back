4742bf8f89c19152cac2470e7e176e36ea53de93
Amr Ibrahim <amramr3434@gmail.com>
2024-07-07 10:15:26 +0300

    edit book with modified date

--- app/Repositories/BookRepository.php
+++ app/Repositories/BookRepository.php
@@ -5,6 +5,8 @@ namespace App\Repositories;
 use App\Models\Book;
 use App\Repositories\Interfaces\BookRepositoryInterface;
 use Illuminate\Http\UploadedFile;
+use Carbon\Carbon;
+
 
 class BookRepository implements BookRepositoryInterface
 {
@@ -25,13 +27,18 @@ class BookRepository implements BookRepositoryInterface
             $path = $data['img']->store('book_images', 'public');
             $data['img'] = $path;
         }
-
+        $data['publication_date'] = Carbon::parse($data['publication_date'])->format('Y-m-d H:i:s');
         return Book::create($data);
     }
 
     public function update($id, array $data)
     {
         $book = $this->findById($id);
+        if (isset($data['img']) && $data['img'] instanceof UploadedFile) {
+            $path = $data['img']->store('book_images', 'public');
+            $data['img'] = $path;
+        }
+        $data['publication_date'] = Carbon::parse($data['publication_date'])->format('Y-m-d H:i:s');
         $book->update($data);
         return $book->load(['author', 'category', 'publisher', 'language']);
     }
