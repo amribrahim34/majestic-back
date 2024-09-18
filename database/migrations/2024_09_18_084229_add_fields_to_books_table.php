0f9059c477fef6982127e2431065b39b16e49164
Amr Ibrahim <amramr3434@gmail.com>
2024-09-18 11:47:16 +0300

    add sort , is_active to books

---
+++ database/migrations/2024_09_18_084229_add_fields_to_books_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->boolean('is_active')->default(true)->before('created_at');
+            $table->unsignedBigInteger('sort')->nullable()->before('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropColumn('is_active');
+            $table->dropColumn('sort');
+        });
+    }
+};
